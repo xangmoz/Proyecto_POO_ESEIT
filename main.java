bd660051a10a0e72ecd9b18cfcf03957b8cce702
Angelo Morales <160273645+xangmoz@users.noreply.github.com>
2026-04-18 17:16:52 -0500

    Se añade Main del proyecto

---
+++ main.java
@@ -0,0 +1,25 @@
+public class main {
+
+     static void main(String[] args){
+
+         // Agregamos productos
+
+        Product product1 = new Product("lentejas", "comestibles", 20, 2500);
+        Product product2 = new Product("res", "carnico", 20, 7000);
+
+        Inventory inventory1 = new Inventory();
+        inventory1.addProduct(product1);
+        inventory1.addProduct(product2);
+
+        // Imprimimos los productos
+        inventory1.queryProduct();
+
+
+        // Agregamos los usuarios
+
+
+
+    }
+
+}
+
