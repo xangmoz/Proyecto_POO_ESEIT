fb0525f9158b77467b765016948c6ecd8e1029e3
xangmoz <xang813@gmail.com>
2026-06-03 15:43:49 -0500

    correcciones en el archivo main

--- main.java
+++ main.java
@@ -1,8 +1,6 @@
-import java.lang.classfile.instruction.SwitchCase;
-
 public class main {
 
-      static void main(String[] args){
+      public static void main(String[] args){
         Inventario tienda = new Inventario();
         Admin jefe = new Admin("Jose", 123, "direccion", tienda);
 
