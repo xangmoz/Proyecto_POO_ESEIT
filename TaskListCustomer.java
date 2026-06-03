ece3c4823ecf1fb3225d7068a5c59f215c7bf951
xangmoz <xang813@gmail.com>
2026-06-03 14:44:11 -0500

    se agregan correcciones en el menu principal

--- TaskListCustomer.java
+++ TaskListCustomer.java
@@ -1,9 +1,9 @@
 public class  TaskListCustomer {
     public void taskList() {
-        System.out.println("BIENVENDO AL SISTEMA DE INVENTARIO");
+        System.out.println("BIENVENIDO AL SISTEMA DE INVENTARIO");
         System.out.println("""
                 ¿Que deseas hacer el dia de hoy?
-                \n1. Registrar Prodcuto
+                \n1. Registrar Producto
                 \n2. Ver inventario
                 \n3. Realizar venta
                 \n4. Salir
